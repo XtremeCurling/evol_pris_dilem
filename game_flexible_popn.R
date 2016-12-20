481e098f91f82f57b3d44c16c0175c54bfd1808a
Daniel Sharp <dsharp@Daniels-MacBook-Air.local>
2016-12-20 04:26:15 -0500

    Add comments, tweak parameters

--- game_flexible_popn.R
+++ game_flexible_popn.R
@@ -1,92 +1,131 @@
+# Run the simulation for T time periods
 game_flexible_popn <- function(T) {
+  ## Set the parameters
+  # Function parameters
   alpha <- 1
   beta <- 5e-3
-  gamma <- 1e4
+  gamma <- 4e4
+  # Individual effort levels
   e_D <- .5
   e_C <- 1
+  # Bonus effort from successful cooperation
   extra_CC <- .25
-  a_C <- .1
-  a_D <- 1-(1-a_C)^(e_D/e_C)
-  p_r <- .35
-  p_m <- 20/(1*16000)
-  R <- .5
-  N_min <- 0
-  N_max <- 100000
-  N_guess <- 1000
-  
+  # Collective effort levels
   e_DD <- e_D
   e_CD <- (e_D+e_C)/2
   e_DC <- e_CD
   e_CC <- (2*e_C+extra_CC)/2
+  # Accident probabilities
+  a_C <- .3
+  a_D <- 1 - (1-a_C)^(e_D/e_C)
+  # Reproduction probability
+  #   Conditional on surviving to the next time period
+  p_r <- .8
+  # Mutation probability
+  p_m <- 20/(1*16000)
+  # Initial resource amount
+  R <- .5
+  # Initial intervals and guess for the steady state population size
+  N_min <- 0
+  N_max <- 100000
+  N_guess <- 1000
+  # Determines the intervals by which game statistics are recorded
+  #   I.e. stats_interval = 5 means stats will be saved for every 5th time period
+  stat_interval <- 5
   
-  while (N_max-N_min>1) {
-    LHS <- 1-(1/alpha)*(1-exp(-beta*N_guess*e_CC))
-    RHS <- -(N_guess/(gamma*(1-exp(-beta*N_guess*e_CC))))*log((p_r-a_c*(1+p_r))/((1-a_c)*(1+p_r)))
+  # Numerically solve for the steady state population size under full cooperation.
+  # The solution will be used as the initial population size.
+  # The LHS and RHS come from a system of equations that was worked out on paper.
+  while (N_max - N_min > 1) {
+    LHS <- 1 - (1/alpha)*(1 - exp(-beta*N_guess*e_CC))
+    RHS <- -(N_guess/(gamma*(1 - exp(-beta*N_guess*e_CC))))*log((p_r - a_C*(1+p_r))/((1-a_C)*(1+p_r)))
     if (RHS > LHS) {
       N_max <- N_guess
     }
     else {
       N_min <- N_guess
     }
-    N_guess <- (N_max+N_min)/2
+    N_guess <- (N_max + N_min)/2
   }
   N <- ceiling(N_guess)
   print(N)
-  #N <- 1000
   
-  surv_prob <- rep(1,N)
-  hist      <- matrix((runif(2*N) > .5),N,2)
-  strat     <- matrix((runif(16*N) > .5),N,16)
-  game_mat  <- cbind(surv_prob,hist,strat)
+  # Initial survival probabilites (all 1)
+  surv_prob <- rep(1, N)
+  # Initial player histories (random binomial, length 2)
+  hist <- matrix((runif(2*N) > .5), nrow = N, ncol = 2)
+  # Initial player strategies i.e. genes (random binomial, length 16)
+  #   There are 16 different possible 2-period histories between 2 players
+  strat <- matrix((runif(16*N) > .5), nrow = N, ncol = 16)
+  # Combine into the "game matrix"
+  game_mat <- cbind(surv_prob, hist, strat)
   
-  powers_vec <- c(8,4,2,1)
-  
-  stat_interval <- 5
-  stats <- matrix(0,floor(T/stat_interval),21)
+  # Initiate the matrix in which simulation statistics will be saved
+  stats <- matrix(0, nrow = floor(T/stat_interval), ncol = 21)
   
   for (t in 1:T) {
-    game_mat  <- game_mat[sample(N),]
+    # Randomly sort the players into pairs
+    game_mat <- game_mat[sample(N), ]
+    # Neven is either N if N even, or N - 1 if N odd.
+    # This ensures that all players are paired in play_flexible_popn().
     Neven <- 2*floor(N/2)
-    play <- play_flexible_popn(game_mat[1:Neven,],Neven,powers_vec,alpha,beta,gamma,R,e_DD,e_CD,e_CC,a_C,a_D)
-    game_mat[1:Neven,] <- play$game_mat
+    # "Play" the round; i.e. allow all people to engage in resource collection
+    #   with their partners, either cooperating (putting in high effort) or
+    #   defecting (putting in low effort)
+    play <- play_flexible_popn(game_mat[1:Neven, ], Neven, powers_vec, alpha,
+                               beta, gamma, R, e_DD, e_CD, e_CC, a_C, a_D)
+    game_mat[1:Neven, ] <- play$game_mat
     E <- play$E
     
+    # Every `stat_interval` periods, record the population's statistics
     if (t/stat_interval == floor(t/stat_interval)) {
-      stats[t/stat_interval,1] <- t
-      stats[t/stat_interval,2] <- sum((1-game_mat[,2]))/(Neven)
+      stats[t/stat_interval, 1] <- t
+      # Percent of population that cooperated in time period `t`
+      stats[t/stat_interval, 2] <- sum((1 - game_mat[,2]))/Neven
     }
     
+    # Every 250 periods, print the time period and the percent of the population
+    # that cooperated.
     if (t/250 == floor(t/250)) {
-      print(c(t,sum((1-game_mat[,2]))/Neven))
+      print(c(t, sum((1 - game_mat[,2]))/Neven))
     }
     
-    is_alive  <- runif(N) <= game_mat[,1]
+    # Determine which players survive to time period `t + 1`
+    is_alive <- runif(N) <= game_mat[, 1]
     num_alive <- sum(is_alive)
-    game_mat  <- game_mat[is_alive,]
+    game_mat  <- game_mat[is_alive, ]
     
-    is_repr  <- runif(num_alive) <= p_r
+    # Determine which players reproduce
+    is_repr <- runif(num_alive) <= p_r
     num_repr <- sum(is_repr)
     
-    children  <- game_mat[is_repr,]
-    is_mutate <- matrix(runif(16*num_repr),nrow=num_repr,ncol=16) <= p_m
-    children[,4:19] <- (children[,4:19]-is_mutate)*(children[,4:19]-is_mutate)
+    # Children of reproducing players, possibly with mutated strategy genes
+    children <- game_mat[is_repr, ]
+    is_mutate <- matrix(runif(16*num_repr), nrow = num_repr, ncol = 16) <= p_m
+    children[, 4:19] <- (children[, 4:19] - is_mutate)*(children[, 4:19] - is_mutate)
+    game_mat <- rbind(game_mat, children)
     
-    game_mat <- rbind(game_mat,children)
-    
+    # New population size
     N <- nrow(game_mat)
     
-    R <- R*(1+alpha*(1-R)-(1-exp(-beta*E)))
+    # New resource amount, based on total time period `t` effort
+    R <- R*(1 + alpha*(1-R) - (1 - exp(-beta*E)))
     
-    #print(c(t,N,R,mean(game_mat[,1])))
-    
+    # Every `stat_interval` periods, record the population's statistics
     if (t/stat_interval == floor(t/stat_interval)) {
-      stats[t/stat_interval,3] <- N
-      stats[t/stat_interval,4] <- R
-      stats[t/stat_interval,5] <- mean(game_mat[,1])
-      stats[t/stat_interval,6:21] <- colSums(game_mat[,4:19])/N
+      # Population size after period `t` deaths and births
+      stats[t/stat_interval, 3] <- N
+      # Resource amount of period `t` consumption and resource growth 
+      stats[t/stat_interval, 4] <- R
+      # Mean survival probability (including children, whose values will
+      #   be equal to those of their parents in period `t`)
+      stats[t/stat_interval, 5] <- mean(game_mat[, 1])
+      # Mean of defections in each of the 16 strategy genes
+      stats[t/stat_interval, 6:21] <- colSums(game_mat[, 4:19])/N
     }
   }
   
+  print(game_mat[1:10, ])
+  # Return the game statistics
   return(stats)
-  
 }
--- play_flexible_popn.R
+++ play_flexible_popn.R
@@ -1,54 +1,72 @@
-play_flexible_popn <- function(game_matrix,Neven,powers_vec,alpha,beta,gamma,R,e_DD,e_CD,e_CC,a_C,a_D) {
+play_flexible_popn <- function(game_matrix, Neven, powers_vec, alpha, beta,
+                               gamma, R, e_DD, e_CD, e_CC, a_C, a_D) {
+  # 2-period player histories
+  hist <- game_matrix[, 2:3]
+  # Odd indices
+  odds  <- seq(1, Neven - 1, 2)
+  # Odd- and even-indexed player histories
+  hist_odd <- hist[odds, ]
+  hist_even <- hist[(odds + 1), ]
   
-  hist <- game_matrix[,2:3]
+  # Matrix with each player's information about her own and partner's histories
+  info <- matrix(NA, nrow = Neven, ncol = 4)
+  # Odd- and even-indexed player information
+  info[odds, ] <- cbind(hist_even[, 1], hist_odd[, 1], hist_even[, 2], hist_odd[, 2])
+  info[(odds + 1), ] <- cbind(hist_odd[, 1], hist_even[, 1], hist_odd[, 2], hist_even[, 2])
   
-  odds  <- seq(1,Neven-1,2)
-  
-  hist1 <- hist[odds,]
-  hist2 <- hist[(odds+1),]
- 
-  info1 <- cbind(hist2[,1],hist1[,1],hist2[,2],hist1[,2])
-  info2 <- cbind(hist1[,1],hist2[,1],hist1[,2],hist2[,2])
-  
-  info <- matrix(NA,nrow=Neven,ncol=4)
-  
-  info[odds,]     <- info1
-  info[(odds+1),] <- info2
-  
+  # Player's information translated to their strategy for the time period
+  #   The strategy is a number 0-15 that determines which strategy to use
+  # Powers of 2
+  powers_vec <- c(8, 4, 2, 1)
+  # powers_vec <- 2^((ncol(info):1) - 1)
   info_to_strat <- colSums(t(info)*powers_vec)
+  # Determine the strategy's index within the `game_matrix`
+  strat_index <- cbind(1:Neven, info_to_strat + 4)
   
-  strat_index <- cbind(1:Neven,info_to_strat+4)
-  
+  # Determine each player's "play", i.e. whether they cooperate or defect 
   play <- game_matrix[strat_index]
+  # Update the players' histories with their current plays
+  game_matrix[, 3] <- game_matrix[, 2]
+  game_matrix[, 2] <- play
   
-  game_matrix[,3] <- game_matrix[,2]
-  game_matrix[,2] <- play
-  
-  play1 <- play[odds]
-  play2 <- play[odds+1]
-  
-  n_CC <- 2*sum((1-play1)*(1-play2))
-  n_CD <- sum((1-play1)*play2)+sum(play1*(1-play2))
+  # Calculate effort
+  # Odd- and even-indexed player plays
+  play_odd <- play[odds]
+  play_even <- play[odds + 1]
+  # Total number of people in each of the 4 types of pairs
+  n_CC <- 2*sum((1 - play_odd)*(1 - play_even))
+  n_CD <- sum((1 - play_odd)*play_even) + sum(play_odd*(1 - play_even))
   n_DC <- n_CD
-  n_DD <- 2*sum(play1*play2)
-  
+  n_DD <- 2*sum(play_odd*play_even)
+  # Total population effort
   e_DC <- e_CD
-  
   E <- n_CC*e_CC + n_CD*e_CD + n_DC*e_DC + n_DD*e_DD
+  # Cumulative efforts exerted by DD, CD/DC, and CC pairs
+  E_DD <- Neven*(e_DD/2)
+  E_CD <- E_DD + (Neven - n_DD)*((e_CD - e_DD)/2)
+  E_CC <- E
   
-  y_DD <- (1/Neven)*R*(1-exp(-beta*Neven*e_DD))
-  y_CD <- (1/(Neven-n_DD))*R*((1-exp(-beta*(Neven*n_DD+(Neven-n_DD)*(e_CD-e_DD))))-(n_DD/N)*(1-exp(-beta*Neven*e_DD)))
+  # Calculate resource consumption
+  # Individual resource consumption for each of the 4 types of pairs
+  y_DD <- (1/Neven)*R*(1 - exp(-beta*E_DD))
+  y_CD <- y_DD + (R/(Neven - n_DD))*(exp(-beta*E_DD) - exp(-beta*E_CD))
   y_DC <- y_CD
-  y_CC <- (1/n_CC)*R*((1-exp(-beta*Neven*E))-((n_CD+n_DC)/(Neven-n_DD))*(1-exp(-beta*(Neven*e_DD+(N-n_DD)*(e_CD-e_DD))))-(n_CC/(Neven-n_DD))*(n_DD/Neven)*(1-exp(-beta*Neven*e_DD)))
+  y_CC <- y_CD + (R/n_CC)*(exp(-beta*E_CD) - exp(-beta*E_CC))
+  # Figure out each specific player's consumption
+  consumption <- rep(NA, Neven)
+  consumption[odds] <- 
+    y_DD*play_odd*play_even + 
+    y_DC*play_odd*(1-play_even) + 
+    y_CD*(1-play_odd)*play_even + 
+    y_CC*(1-play_odd)*(1-play_even)
+  # Partners consume the same amount
+  consumption[odds + 1] <- consumption[odds]
   
-  consumption <- rep(NA,Neven)
-  consumption[odds]   <- y_DD*play1*play2 + y_DC*play1*(1-play2) + y_CD*(1-play1)*play2 + y_CC*(1-play1)*(1-play2)
-  consumption[odds+1] <- consumption[odds]
+  # Calculate each player's survival probability, based on consumption and
+  # accident probability (as determined by effort)
+  surv_prob <- (1 - exp(-gamma*consumption))*(1 - play*a_D - (1-play)*a_C)
+  game_matrix[, 1] <- surv_prob
   
-  surv_prob <- (1-exp(-gamma*consumption))*(1-play*a_D-(1-play)*a_C)
-  
-  game_matrix[,1] <- surv_prob
-  
-  return(list(game_mat=game_matrix,E=E))
-  
+  # Return the game matrix and total effort level
+  return(list(game_mat = game_matrix, E = E))
 }
